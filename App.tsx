d7daa8731760ca596c3535575e7024b7c7f46445
Michele Bonazza <michele@sketchtogether.com>
2019-02-18 18:02:19 +0100

    Add call to provider server

--- App.tsx
+++ App.tsx
@@ -5,23 +5,29 @@ import { colors } from "Urbi/utils/colors";
 import { Font, Linking } from "expo";
 import ButtonPrimary from "Urbi/molecules/buttons/ButtonPrimary";
 
+const caBaseUrl = "https://urbitunnel.eu.ngrok.io";
+
 type State = {
   fontsLoaded: boolean;
-  response: string;
+  walletResponse: string;
 };
 
 export default class App extends React.Component<{}, State> {
   constructor(props: any) {
     super(props);
-    this.state = { fontsLoaded: false, response: "" };
+    this.state = { fontsLoaded: false, walletResponse: "" };
     this.onSubmit = this.onSubmit.bind(this);
-    AppState.addEventListener("change", state => {
-      if (state === "active") {
-        Linking.parseInitialURLAsync().then(info => {
-          if (info.queryParams.payload) {
-            this.setState({ response: info.queryParams.payload });
-          }
-        });
+    Linking.addEventListener("url", info => {
+      if (info) {
+        const { queryParams } = Linking.parse(info.url);
+        if (queryParams.payload) {
+          this.setState({ walletResponse: queryParams.payload });
+        }
+      }
+    });
+    Linking.parseInitialURLAsync().then(info => {
+      if (info.queryParams.payload) {
+        this.setState({ walletResponse: info.queryParams.payload });
       }
     });
   }
@@ -37,24 +43,46 @@ export default class App extends React.Component<{}, State> {
     this.setState({ fontsLoaded: true });
   }
 
-  onSubmit() {
-    const callback = encodeURIComponent(Linking.makeUrl("callback"));
-    // const url = `urbiwallet://consent/bobcars/${callback}`;
-    const url = `urbiwallet://consent/bobcars/${callback}`;
-    Linking.openURL(url);
+  async onSubmit() {
+    const { walletResponse } = this.state;
+    if (walletResponse.length > 2) {
+      const response = await fetch(`${caBaseUrl}/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: walletResponse
+      });
+      if (response.status !== 200) {
+        window.alert(`Our server sent us back a code ${response.status}`);
+      } else {
+        const json = await response.json();
+        window.alert(`Response from server:\n${JSON.stringify(json)}`);
+      }
+    } else {
+      const callback = encodeURIComponent(Linking.makeUrl("callback"));
+      // const url = `urbiwallet://consent/bobcars/${callback}`;
+      // const url = `urbiwallet://consent/bobcars/${callback}`;
+      // const url = `exp://192.168.2.184:19000/--/consent/bobcars/${callback}`;
+      const url = `exp://exp.host/@michele.bon/urbi-wallet/--/consent/bobcars/${callback}`;
+      Linking.openURL(url);
+    }
   }
 
   render() {
-    if (!this.state.fontsLoaded) return null;
+    const { fontsLoaded, walletResponse } = this.state;
+    if (!fontsLoaded) return null;
+    const responseFetched = walletResponse.length > 2;
     return (
       <View style={styles.Container}>
         <Text style={styles.Title}>Welcome to BobCars!</Text>
         <View style={styles.BottomButton}>
-          <ButtonPrimary label="Get data from urbi!" onPress={this.onSubmit} />
+          {responseFetched ? (
+            <Text style={styles.Info}>Data fetched from Urbi 👌</Text>
+          ) : null}
+          <ButtonPrimary
+            label={responseFetched ? "Sign up" : "Get data from urbi"}
+            onPress={this.onSubmit}
+          />
         </View>
-        {this.state.response.length > 2 ? (
-          <Text style={styles.Info}>{this.state.response}</Text>
-        ) : null}
       </View>
     );
   }
@@ -71,7 +99,9 @@ const styles = StyleSheet.create({
     ...makeTextStyle("hero", colors.brand)
   },
   Info: {
-    ...makeTextStyle("title", colors.uma)
+    ...makeTextStyle("title", colors.uma),
+    textAlign: "center",
+    marginBottom: 10
   },
   BottomButton: {
     padding: 20
